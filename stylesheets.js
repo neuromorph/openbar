27d02ec59efb2ff4093d2acb07241bb1a94a3955
Pradeep Gaidhani <pradeep.gaidhani@gmail.com>
2024-05-13 10:02:10 +0530

    openbar2.0: add color util functions

--- stylesheets.js
+++ stylesheets.js
@@ -365,6 +365,66 @@ function rgbToHex(r, g, b) {
     return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
 }
 
+// Add tint to RGB color
+function addTint(rgbColor, amount) {
+    const [r, g, b] = rgbColor.map(val => val + (255 - val) * amount);
+    return [r, g, b];
+}
+
+// Add shade to RGB color - modified (grey)
+function addShade(rgbColor, amount, target=0) {
+    const [r, g, b] = rgbColor.map(val => val + (target - val) * amount);
+    return [r, g, b];
+}
+
+// Converts RGB to HSL
+function rgbToHsl(rgb) {
+    let [r, g, b] = [rgb[0]/255, rgb[1]/255, rgb[2]/255];
+    let max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
+    if(max == min) {
+        h = s = 0; // achromatic
+    } else {
+        let d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    return [h, s, l]; // h, s, l in range 0 - 1
+}
+
+// Converts HSL to RGB
+function hslToRgb(hsl) {
+    let [h, s, l] = hsl;
+    let r, g, b;
+  
+    if (s === 0) {
+        r = g = b = l; // achromatic
+    } else {
+        const hue2rgb = (p, q, t) => {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+            return p;
+        };
+  
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+  
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+  
+    return [r * 255, g * 255, b * 255];
+}
+
 // Generate stylesheet string and save stylesheet file
 function saveStylesheet(obar, Me) {
 
