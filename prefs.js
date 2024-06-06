3d408a144d285b5b5a1e7f3b3622f85b8a14b78e
Pradeep G <pradeep.gaidhani@gmail.com>
2024-06-06 11:41:00 +0530

    openbar2.0: fix - window size defaults

--- prefs.js
+++ prefs.js
@@ -356,8 +356,8 @@ class OpenbarPrefs {
     fillOpenbarPrefs(window, openbar) {
 
         window.default_width = 825;
-        window.default_height = 910;        
-        window.set_size_request(815, 880);
+        window.default_height = 915;        
+        window.set_size_request(815, 885);
 
         window.paletteButtons = [];
         window.colorButtons = [];
