f54eb2a419f923f4f36ecebc2293b90d214b7764
Pradeep G <pradeep.gaidhani@gmail.com>
2024-09-07 19:31:19 +0530

    fix: overview search-entry border opacity

--- stylesheets.js
+++ stylesheets.js
@@ -3141,7 +3141,7 @@ function getStylesheet(obar, Me) {
             border: 2px solid ${mhbg} !important;
         }
         StEntry .search-entry {
-            border: 2px solid rgba(${smfgred},${smfggreen},${smfgblue},0.6) !important;
+            border: 2px solid rgba(${smfgred},${smfggreen},${smfgblue},0.5) !important;
         }
         .system-action-icon {
             background-color: rgba(0, 0, 0, 0.8);
